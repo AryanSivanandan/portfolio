6f80e3f11c2da866b9af01ab146c3a3f09a3ebf6
Aryan Sivanandan <aryan.sivanandan04@gmail.com>
2026-02-04 00:58:12 +0530

    Temp admin

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "portfolio_app",
+    "portfolio_app.apps.PortfolioAppConfig",
 ]
 
 MIDDLEWARE = [
--- portfolio_app/apps.py
+++ portfolio_app/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class PortfolioAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio_app'
+
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="aryan.sivanandan04@gmail.com",
+                password="admin1234"
+            )
